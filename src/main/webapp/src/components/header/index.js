73e6557641749d72ffd261191ef3c9e4ab88be58
hmachahary <hmachahary@gmail.com>
2022-05-11 19:53:17 +0530

    Fix page layout and profile image added

--- src/main/webapp/src/components/header/index.js
+++ src/main/webapp/src/components/header/index.js
@@ -1,11 +1,13 @@
 import "./styles.css";
-
+import profile from "../../assets/images/profile.jpg";
 export default function Header() {
 	return (
 		<header className="wf_header">
 			<div className="wf_header-container">
 				<div className="wf_logo">CODE RED</div>
-				<div className="wf_profile">Profile</div>
+				<div className="wf_profile">
+					<img className="wf_profile-img" src={profile} alt="Profile pic" />
+				</div>
 			</div>
 		</header>
 	);
