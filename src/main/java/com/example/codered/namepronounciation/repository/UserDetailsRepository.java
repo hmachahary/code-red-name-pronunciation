298d689bb369c99a474538ed92221e67711f46bf
unknown <taresh_uppal@hotmail.com>
2022-05-15 00:07:19 +0530

    all user details fetched and integrated

--- src/main/java/com/example/codered/namepronounciation/repository/UserDetailsRepository.java
+++ src/main/java/com/example/codered/namepronounciation/repository/UserDetailsRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface UserDetailsRepository extends JpaRepository<UserDetails,String> {
 
     @Query(value = "UPDATE T_USER_DETAILS SET  NAME = ?1, DOB =?2, COUNTRY =?3, OFFICE_ADDRESS = ?4, RESEDENTIAL_ADDRESS = ?5, PHONE =?6, SKILLS=?7, DESIGNATION =?8, MODIFIED_BY =?10, MODIFIED_TS =?11 WHERE EMAIL = ?9", nativeQuery = true)
     void updateProfile(String name, Date dob, String country, String officeAddress, String residentialAdress, String phone, String skills, String designation, String email, Date modifiedBy, Date modifiedAt);
+
+    List<UserDetails> findAll();
 }
