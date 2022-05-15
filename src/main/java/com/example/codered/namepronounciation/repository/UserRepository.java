f98561f34fb9442a6e8b286043f8ce3b1a11ebeb
Arnapurna <arnapurnasatapathy1@gmail.com>
2022-05-15 11:47:41 +0530

    user login by email added

--- src/main/java/com/example/codered/namepronounciation/repository/UserRepository.java
+++ src/main/java/com/example/codered/namepronounciation/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.codered.namepronounciation.repository;
 import com.example.codered.namepronounciation.dbEntity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<Users, String> {
+    Optional<Users> findByEmailIgnoreCase(String email);
 }
