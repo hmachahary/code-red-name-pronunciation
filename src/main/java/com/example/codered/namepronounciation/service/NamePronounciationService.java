68d520f575355054f04483f3ad4df7deb0636054
hmachahary <hmachahary@gmail.com>
2022-05-15 09:03:22 +0530

    Voice presets added to db and rest api exposed

--- src/main/java/com/example/codered/namepronounciation/service/NamePronounciationService.java
+++ src/main/java/com/example/codered/namepronounciation/service/NamePronounciationService.java
@@ -1,10 +1,13 @@
 package com.example.codered.namepronounciation.service;
 
+import com.example.codered.namepronounciation.dbEntity.SpeechPresets;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @Service
 public interface NamePronounciationService {
     void editPronounciation(String email, byte[] audioBuffer) throws SQLException;
+    List<SpeechPresets> getAllVoicePresets();
 }
