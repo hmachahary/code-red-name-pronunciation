9e228cebe81efa70f17a1336646b35daa15d2d1f
hmachahary <hmachahary@gmail.com>
2022-05-17 01:04:22 +0530

    Redirect after edit

--- src/main/webapp/src/actions/employee.js
+++ src/main/webapp/src/actions/employee.js
@@ -4,10 +4,10 @@ const api = axios.create();
 api.defaults.baseURL = ""; // URL here...
 const headers = { "Content-Type": "applictation/json" };
 
-export const getEmployeeData = async () => {	
+export const getEmployeeData = async () => {
 	return api
-		.get("http://localhost:8080/api/v1/users", { headers: headers })		.then((response) => {	
-					
+		.get("http://localhost:8080/api/v1/users", { headers: headers })
+		.then((response) => {
 			if (response.status === 200) {
 				return {
 					status: 200,
