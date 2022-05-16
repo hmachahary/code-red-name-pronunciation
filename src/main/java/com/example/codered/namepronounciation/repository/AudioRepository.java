627e8c7446a5450833fdaff6de46149e0e64db61
Arnapurna <arnapurnasatapathy1@gmail.com>
2022-05-16 12:57:55 +0530

    edit functionality fixed

--- src/main/java/com/example/codered/namepronounciation/repository/AudioRepository.java
+++ src/main/java/com/example/codered/namepronounciation/repository/AudioRepository.java
@@ -2,14 +2,18 @@ package com.example.codered.namepronounciation.repository;
 
 import com.example.codered.namepronounciation.dbEntity.AudioTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface AudioRepository extends JpaRepository<AudioTable,String> {
-    @Query(value = "UPDATE TABLE T_NAME_PRONOUNCIATION SET VOICE_NOTE= ?2, REGION = ?3, VOICE_GENDER = ?5, VOICE_TYPE = ?4, PREFERENCE = ?6 WHERE EMAIL =?1", nativeQuery = true)
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE T_NAMES_PRONOUNCIATION SET VOICE_NOTE= ?2, REGION = ?3, VOICE_GENDER = ?5, VOICE_TYPE = ?4, PREFERENCE = ?6 WHERE EMAIL =?1", nativeQuery = true)
     void updateAudio(String email, byte[] voiceNote, String region, String voiceType, String voiceGender, String preference);
 
     List<AudioTable> findAll();
